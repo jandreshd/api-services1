e387231bb52f5b2c32c3b6ab281d47467f042d64
jandreshd <jandreshd@gmail.com>
2021-04-19 21:27:12 -0500

    Version Lista

--- Services.py
+++ Services.py
@@ -1,17 +1,26 @@
+from conexionBD import *
+
 def ping():
     print("pong!")
 
-def add_quote():
-    return 0
+#agregar frase
+def add_quote(body):
+    return guardarFrase(body)
 
+#obtener frase aleatoria
 def get_random_quote():
-    return 0
+    return obtenerFraseAleatoria()
 
-def up_vote():
-    return 0
+#Para lo sgtes 2 metodos crear en lbd mongodb la estrctura 
+#para acumular la cantidad de votos
+#Voto me gusta = Sumar un voto
+def up_vote(id):
+    return actualizarVoto(id, 1)
 
-def down_vote():
-    return 0
+#Voto me gusta = Restar un voto
+def down_vote(id):
+    return actualizarVoto(id, -1)
 
-def delete_quote_by_id():
-    return 0
+#Eliminar frase
+def delete_quote_by_id(id):
+    return eliminarFrase(id)
---
+++ conexionBD.py
@@ -0,0 +1,64 @@
+import pymongo
+import random
+from pymongo import errors as mongoerrors
+
+clientMongo = pymongo.MongoClient("mongodb+srv://jandreshd:ValeIsa2405@clspython.klugh.mongodb.net/db2?retryWrites=true&w=majority")
+db = clientMongo.db2
+
+def consultarCollecionesBD():
+    for i in db.list_collection_names():
+        print (i)
+
+def guardarFrase(body):
+    try:
+        nroFrase = db.misFrases.count_documents({})   
+        datosFrase = db.misFrases.find_one({'frase': body["frase"]})
+        if datosFrase is None:
+            documentoJSON = {'_id': nroFrase + 1, 'frase': body["frase"], 'votos': 0}
+            db.misFrases.insert_one(documentoJSON)
+            return "Frase Agregada"
+        else:
+            return "Frase ya existe"
+    except:
+        print("Error insertando registro en la BD")
+
+def obtenerFraseAleatoria():
+    try:
+        totalFrases = db.misFrases.count_documents({})   
+        query = {'_id': random.randint(1, totalFrases)}
+        datosFrase = db.misFrases.find_one(query)
+        dic = {"frase":datosFrase["frase"]}
+        if datosFrase["votos"] > 0 :
+            dic = {"frase":datosFrase["frase"], "votos": datosFrase["votos"]}
+        return dic
+    except:
+        return "Error consultando la frase del dia"
+
+def actualizarVoto(id, cantidad):
+    try:
+        query = {'_id': id}
+        datosFrase = db.misFrases.find_one(query)
+        if datosFrase is None:
+            return "Frase No Encontrada"
+        else:
+            datosFrase["votos"] += cantidad
+            if datosFrase["votos"] > 0 :
+                votos = { "$set": { "votos": datosFrase["votos"] } }
+            else:
+                votos = { "$set": { "votos": 0} }
+            db.misFrases.update_one(query, votos)
+            return "Actualizacion Frase Exitosa"
+    except:
+        return  "Error actualizando registro en la BD"
+
+def eliminarFrase(id):
+    try:
+        query = {'_id': id}
+        datosFrase = db.misFrases.find_one(query)
+        if datosFrase is None:
+            return "Frase No Encontrada"
+        else:
+            db.misFrases.delete_one(query)
+            return "Frase Eliminada"
+    except:
+        return "Error eliminando registro en la BD"
